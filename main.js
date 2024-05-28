dabcb15131a5a47e2d746d918f9135053a58d121
shukriddinovs <sshukriddinov524@gmail.com>
2024-05-28 14:43:52 +0500

    {.}

---
+++ main.js
@@ -0,0 +1,8 @@
+// let title = document.getElementsByClassName("dom");
+// title.remove();
+
+let random ;
+random= Math.floor(Math.random() * 100)
+let h1 = document.querySelector("h1")
+
+h1.innerHTML = random
